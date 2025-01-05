df5a79e8f3c2ccfaed099a00c74bdc72521d3e8d
pragna <pragnagoud1999@gmail.com>
2025-01-05 21:08:22 +0530

    GitHubpractise first commit

---
+++ src/test/java/extentreports/extentreports/ExtentReportDemo.java
@@ -0,0 +1,37 @@
+package extentreports.extentreports;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportDemo {
+	ExtentReports extent;
+
+	@BeforeTest
+	public void config() {
+		String path=System.getProperty("user.dir")+"//reports//indexreport.html";
+		ExtentSparkReporter reporter= new ExtentSparkReporter(path);
+		reporter.config().setDocumentTitle("Test Results");
+		reporter.config().setReportName("Web automation test results");
+		extent= new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Pragna");
+	}
+
+	@Test
+	public void initialDriver() {
+		ExtentTest test=extent.createTest("Initial Demo");
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://rahulshettyacademy.com/#/practice-project");
+		System.out.println(driver.getTitle());
+		test.fail("we failed the test expliciltly ");
+		extent.flush();
+
+	}
+
+}
